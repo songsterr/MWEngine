3b97eedf55026cc575ecf79dc36be0bef17793df
Андрей Ставринов <andrey@stavrinov.info>
2015-11-11 00:31:08 +0300

    Add swig execution step to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # custom to your machine : Android NDK library location
-export ANDROID_NDK_ROOT=/Library/AndroidNDK
+#export ANDROID_NDK_ROOT=/Library/AndroidNDK
 
 # the input folders for the C++ sources
 INPUT="-I./jni"
@@ -9,7 +9,7 @@ INPUT="-I./jni"
 rm -rf src/nl/igorski/lib/audio/nativeaudio
 mkdir -p src/nl/igorski/lib/audio/nativeaudio
 
-swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
+/usr/local/bin/swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
 
 $ANDROID_NDK_ROOT/ndk-build TARGET_PLATFORM=android-14 V=1
 

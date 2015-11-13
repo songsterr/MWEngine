95e03ef77013be031aa4de74c4e3ec11b3261ff7
Андрей Ставринов <andrey@stavrinov.info>
2015-11-13 13:34:57 +0300

    Change android ndk locations env variable name

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# custom to your machine : Android NDK library location
-#export ANDROID_NDK_ROOT=/Library/AndroidNDK
-
 # the input folders for the C++ sources
 INPUT="-I./jni"
 
@@ -11,7 +8,8 @@ mkdir -p src/nl/igorski/lib/audio/nativeaudio
 
 /usr/local/bin/swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
 
-$ANDROID_NDK_ROOT/ndk-build TARGET_PLATFORM=android-14 V=1
+$ANDROID_NDK_HOME/ndk-build TARGET_PLATFORM=android-14 V=1
+
 
 
 

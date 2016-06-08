0d5fe0cc26b1961f7ed917d074d5fff80188f2c8
Андрей Ставринов <andrey@stavrinov.info>
2016-06-08 14:39:39 +0200

    Add logging to building script (to debug strange behaviour on jenkins)

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 # the input folders for the C++ sources
 INPUT="-I./jni"
 
-rm -rf src/main/java/nl/igorski/lib/audio/nativeaudio
+echo Cleaning swig-generated java sources...
+
+rm -rfv src/main/java/nl/igorski/lib/audio/nativeaudio
 mkdir -p src/main/java/nl/igorski/lib/audio/nativeaudio
 
+echo Generating java sources with SWIG...
 swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/main/java/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
 
+echo NDK Build...
 $ANDROID_NDK_HOME/ndk-build TARGET_PLATFORM=android-14 V=1 $1

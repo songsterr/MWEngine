6e9d28bc9eb0ee62bb33a914671bfe99ffac38e7
Андрей Ставринов <andrey@stavrinov.info>
2016-06-08 15:45:36 +0200

    Fix swig path

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rm -rfv src/main/java/nl/igorski/lib/audio/nativeaudio
 mkdir -p src/main/java/nl/igorski/lib/audio/nativeaudio
 
 echo Generating java sources with SWIG...
-swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/main/java/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
+/usr/local/bin/swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/main/java/nl/igorski/lib/audio/nativeaudio -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
 
 echo NDK Build...
 $ANDROID_NDK_HOME/ndk-build TARGET_PLATFORM=android-14 V=1 $1
